a86148157bdd0b3d8047ed9ed23c70d3b160ee96
steven <sg.lesaux@gmail.com>
2021-12-16 19:48:50 +0100

    Avancement de l'integration de la page produit

--- front/js/product_page.js
+++ front/js/product_page.js
@@ -4,22 +4,37 @@ const price = document.getElementById("price");
 const description = document.getElementById("description");
 const colors = document.getElementById("colors");
 
+let productDetails = [];
 const fetchProducts = async () => {
   await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => {
-      let productDetails = [];
       productDetails = data;
-      console.log(productDetails);
       displayProducts();
+      
     });
 };
-fetchProducts();
-
-function displayDetails() {
+function displayProducts() {
   let html = "";
-  productDetails.forEach((detail) => {
-    html += `${detail.imageUrl} " " ${detail.altTxt}`;
+  productDetails.forEach((product) => {
+    itemImg.innerHTML = `
+      <img src="${data.imageUrl}" alt="${data.altTxt}">
+      `;
+      title.innerHTML = `
+      <h1 id="title">${data.name}</h1>
+      `;
+      price.innerHTML = `
+      <span id="price">${data.price}</span>
+      `;
+      description.innerHTML = `
+      <p id="description">${data.description}</p>
+      `;
+      colors.innerHTML =`
+      <select name="color-select" id="colors">
+      <option value="">--SVP, choisissez une couleur --</option>
+      <option value="vert">${data.colors.green}</option>
+      <option value="blanc">${data.colors.white}</option> -->
+      </select>
+      `
   });
-  itemImg.innerHTML = html;
-}
+  htmlProducts.innerHTML = html;
